31d55ec1af487d18956a622e774864d5712803c0
Adrian <adrian.stypinski@gmail.com>
2022-06-02 21:39:21 +0200

    PZG2-123 | Added modal

--- frontend/src/GroupList.jsx
+++ frontend/src/GroupList.jsx
@@ -1,6 +1,9 @@
-import {AppShell, Button, Header, Navbar, Table, Text, UnstyledButton} from "@mantine/core";
+import {AppShell, Button, Header, Input, Modal, Navbar, Table, Text, UnstyledButton} from "@mantine/core";
+import {useState} from "react";
 
 const GroupList = () => {
+    const [opened, setOpened] = useState(false);
+
     const header = <Header height={40} p={"sm"}>
         <span>Dziennik elektroniczny</span>
     </Header>
@@ -38,11 +41,11 @@ const GroupList = () => {
             <span className={'group-list-title'}>Lista grup szkolnych</span>
 
             <div>
-                <Button>Utwórz klase</Button>
+                <Button onClick={() => setOpened(true)}>Utwórz klase</Button>
             </div>
 
-            { noGroups && <span>Brak klas</span> }
-            { !noGroups && <div>
+            {noGroups && <span>Brak klas</span>}
+            {!noGroups && <div>
                 <Table
                     striped
                     highlightOnHover
@@ -50,27 +53,48 @@ const GroupList = () => {
                 >
                     <caption>Klasy</caption>
                     <thead>
-                        <tr>
-                            <th className={"group-list-table-id"}>Id</th>
-                            <th className={"group-list-table-name"}>Nazwa</th>
-                            <th className={"group-list-table-year"}>Rocznik</th>
-                            <th className={"group-list-table-modify"}>Modyfikuj</th>
-                            <th className={"group-list-table-remove"}>Usuń</th>
-                        </tr>
+                    <tr>
+                        <th className={"group-list-table-id"}>Id</th>
+                        <th className={"group-list-table-name"}>Nazwa</th>
+                        <th className={"group-list-table-year"}>Rocznik</th>
+                        <th className={"group-list-table-modify"}>Modyfikuj</th>
+                        <th className={"group-list-table-remove"}>Usuń</th>
+                    </tr>
                     </thead>
                     <tbody>
-                        {groups.map((group, index) => <tr key={index}>
-                            <td>{group.id}</td>
-                            <td>{group.name}</td>
-                            <td>{group.schoolYear}</td>
-                            <td><Button>Modyfikuj</Button></td>
-                            <td><Button>Usuń</Button></td>
-                        </tr>)}
+                    {groups.map((group, index) => <tr key={index}>
+                        <td>{group.id}</td>
+                        <td>{group.name}</td>
+                        <td>{group.schoolYear}</td>
+                        <td><Button>Modyfikuj</Button></td>
+                        <td><Button>Usuń</Button></td>
+                    </tr>)}
                     </tbody>
                 </Table>
-            </div> }
+            </div>}
         </div>
     </>
+
+    const modal = <Modal
+        opened={opened}
+        onClose={() => setOpened(false)}
+        title="Utwórz klasę"
+    >
+        <div className="create-modal-content">
+            <Input
+                placeholder="Nazwa klasy"
+            />
+
+            <div className="create-modal-button-container">
+                <Button>
+                    Utwórz klasę
+                </Button>
+                <Button variant="outline" color="red">
+                    Anuluj
+                </Button>
+            </div>
+        </div>
+    </Modal>
 
     return <AppShell
         fixed
@@ -80,7 +104,8 @@ const GroupList = () => {
         padding={"sm"}
     >
         {content}
+        {modal}
     </AppShell>
 }
 
-export { GroupList };
+export {GroupList};
